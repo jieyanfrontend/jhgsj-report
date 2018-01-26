5464f05ea99d4c550eef0e5a9e4632ecd1035f55
lifuzhao100 <993053701@qq.com>
2018-01-27 00:31:28 +0800

    '修改project.config.json文件，设置dist文件夹为小程序主目录'

---
+++ dist/test/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var a = function a() {
+    console.log('a');
+};
+var b = function b() {
+    console.log('b');
+};
+exports.default = a;
+exports.b = b;

d904857af093bcd27caa8c201cbfe44d6d569443
rush2333 <vipersrush@gmail.com>
2018-01-27 21:01:05 +0800

    second

--- dist/pages/photo1/photo1.js
+++ dist/pages/photo1/photo1.js
@@ -1,26 +1,28 @@
+'use strict';
+
 Page({
     data: {
-        message:'请拍摄并上传工作场所',
-        files: [],
-        bShow:true
+        message: '拍照并上传工作环境',
+        files: []
     },
-    chooseImage: function (e) {
+    chooseImage: function chooseImage(e) {
+        var that = this;
         wx.chooseImage({
             count: 1,
             sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
             sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-            success: function (res) {
+            success: function success(res) {
                 // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
                 that.setData({
                     files: that.data.files.concat(res.tempFilePaths)
                 });
             }
-        })
+        });
     },
-    previewImage: function(e){
+    previewImage: function previewImage(e) {
         wx.previewImage({
             current: e.currentTarget.id, // 当前显示图片的http链接
             urls: this.data.files // 需要预览的图片http链接列表
-        })
+        });
     }
 });

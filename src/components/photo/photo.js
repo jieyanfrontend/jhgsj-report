98ff35d206ea172c1a30f29336ca9b3d9282d404
rush2333 <vipersrush@gmail.com>
2018-01-29 16:55:46 +0800

    组件

---
+++ src/components/photo/photo.js
@@ -0,0 +1,23 @@
+// src/components/photo/photo.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    message:""
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})

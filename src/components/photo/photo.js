d5ab7685d084b2fce23a343656f437bdfdb417e0
rush2333 <vipersrush@gmail.com>
2018-01-30 17:12:48 +0800

    组件2

--- src/components/photo/photo.js
+++ src/components/photo/photo.js
@@ -4,20 +4,50 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    message: {
+      type:String,
+      value:'init value'
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    message:""
+      files: []
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
-  }
+      chooseImage: function (e) {
+          var that = this;
+          wx.chooseImage({
+              sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+              sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+              success: function (res) {
+                  // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+                  that.setData({
+                      files: res.tempFilePaths,
+                  });
+              }
+          })
+      },
+      fontSize: function() {
+          wx.navigateTo({
+              url: '../photo1/photo1'
+          })
+      },
+      backSize: function() {
+          wx.navigateTo({
+              url: '../photo2/photo2'
+          })
+      }
+  },
+      workCondition: function() {
+          wx.navigateTo({
+              url: '../photo3/photo3'
+          })
+      }
 })

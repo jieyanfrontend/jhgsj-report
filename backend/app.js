fce8b74713b9cace15dbb3cf2bbdfc44a32dbab4
lifuzhao100 <993053701@qq.com>
2018-02-04 14:06:11 +0800

    '强制转换http为https'

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const Koa = require('koa');
+const enforeHttps = require('koa-sslify');
+const fs = require('fs');
 const http = require('http');
+const https = require('https');
 const { nextRouter } = require('./router');
 const app = new Koa();
 
 app
+    .use(enforeHttps())
     .use(nextRouter.routes())
     .use(nextRouter.allowedMethods());
-http.createServer(app.callback()).listen(80);
+const options = {
+    key: fs.readdirSync('./ssl/2_lifuzhao100.cn.key'),
+    cert: fs.readdirSync('./ssl/1_lifuzhao100.cn_bundle.crt')
+};
+http.createServer( app.callback() ).listen(80);
+https.createServer( options, app.callback()).listen(443);

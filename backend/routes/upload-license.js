12af9b2392949bcfbff10876ee17c5831b332003
lifuzhao100 <993053701@qq.com>
2018-02-05 22:54:31 +0800

    '添加上传图片接口'

--- backend/routes/upload-license.js
+++ backend/routes/upload-license.js
@@ -1,20 +1,12 @@
 let { prevRouter } = require('../config/router');
-let Busboy = require('busboy');
-let fs = require('fs');
-let { resolve } = require('path');
+let multer = require('koa-multer');
 let pool = require('../config/mysql');
+let { resolve } = require('path');
+let upload = multer({
+    dest:  resolve(__dirname, '../images/license')
+});
 let checkRequireParams = require('../utils/checkRequireParams');
-prevRouter.post('/uploadLicense', (ctx, next) => {
-    console.log(ctx.req);
-    let busboy = new Busboy({
-        headers: ctx.req.headers
-    });
-    busboy.on('file', (fieldName, file, fileName, encoding, mimeType) => {
-        console.log("fieldName", fieldName);
-    });
-    busboy.on('field', (fieldName, value, fieldNameTruncated, valueTruncated) => {
-        console.log("fieldName: ", fieldName);
-        console.log("value: ", value);
-    });
-    ctx.req.pipe(busboy);
+prevRouter.post('/uploadLicense', upload.single('file') , (ctx, next) => {
+
+    console.log(ctx.file);
 });

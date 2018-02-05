ad61b0f6d4f63cdce5d27803beb7071ec873aaa9
lifuzhao100 <993053701@qq.com>
2018-02-05 18:35:25 +0800

    '添加上传图片接口'

--- backend/routes/upload-license.js
+++ backend/routes/upload-license.js
@@ -5,6 +5,7 @@ let { resolve } = require('path');
 let pool = require('../config/mysql');
 let checkRequireParams = require('../utils/checkRequireParams');
 prevRouter.post('/uploadLicense', (ctx, next) => {
+    console.log(ctx.req.headers);
     let busboy = new Busboy({
         headers: ctx.req.headers
     });

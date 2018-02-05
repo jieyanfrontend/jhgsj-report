d2bdbabfda9d61a3c7d22d873067155c60f41b62
lifuzhao100 <993053701@qq.com>
2018-02-05 23:10:26 +0800

    '添加上传图片接口'

--- backend/routes/upload-license.js
+++ backend/routes/upload-license.js
@@ -6,7 +6,7 @@ let upload = multer({
     dest:  resolve(__dirname, '../images/license')
 });
 let checkRequireParams = require('../utils/checkRequireParams');
-prevRouter.post('/uploadLicense', upload.single('file') , (ctx, next) => {
+prevRouter.post('/uploadLicense', upload.array('file', 2) , (ctx, next) => {
     console.log("ctx", ctx);
     // console.log("file", ctx.file);
 });

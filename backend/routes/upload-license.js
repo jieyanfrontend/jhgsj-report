457c7b7aed6a1c560e6d49bea96f27ef43b55404
lifuzhao100 <993053701@qq.com>
2018-02-06 00:26:08 +0800

    '添加上传图片接口'

--- backend/routes/upload-license.js
+++ backend/routes/upload-license.js
@@ -3,6 +3,20 @@ let multer = require('koa-multer');
 let pool = require('../config/mysql');
 let { resolve,extname  } = require('path');
 let checkRequireParams = require('../utils/checkRequireParams');
+
+let storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, resolve(__dirname, '../images/license'))
+    },
+    filename: function(req, file, cb){
+        let id = req.body.id;
+        let ext = extname(file.originalname);
+        cb(null, 'license-' + id + ext);
+    }
+});
+let upload = multer({
+    storage: storage
+});
 prevRouter.post('/uploadLicense', upload.any() , (ctx, next) => {
     let req = ctx.req;
     let requireParmas = ['id', 'picture'];
@@ -12,18 +26,6 @@ prevRouter.post('/uploadLicense', upload.any() , (ctx, next) => {
     };
     let ret = checkRequireParams(requireParmas, query);
     if(!ret.errcode){
-        let storage = multer.diskStorage({
-            destination: function(req, file, cb){
-                cb(null, resolve(__dirname, '../images/license'))
-            },
-            filename: function(req, file, cb){
-                let ext = extname(file.originalname);
-                cb(null, 'license-' + query[id] + ext);
-            }
-        });
-        let upload = multer({
-            storage: storage
-        });
 
     }else{
         ctx.body = ret;

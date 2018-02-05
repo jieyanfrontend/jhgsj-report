301da11ec20ff383d49e8b142a695c95bdf9dabb
lifuzhao100 <993053701@qq.com>
2018-02-05 18:20:28 +0800

    '添加上传图片接口'

--- backend/routes/upload-license.js
+++ backend/routes/upload-license.js
@@ -4,15 +4,15 @@ let fs = require('fs');
 let { resolve } = require('path');
 let pool = require('../config/mysql');
 let checkRequireParams = require('../utils/checkRequireParams');
-prevRouter.post('/uploadLicense', async (ctx, next) => {
+prevRouter.post('/uploadLicense', (ctx, next) => {
     console.log(ctx.request);
     let busboy = new Busboy({
-        header: ctx.req.header
+        headers: ctx.req.headers
     });
     busboy.on('file', (fieldName, file, fileName, encoding, mimeType) => {
         console.log(fieldName);
         file.on('data', (data) => {
-            fs.createWriteStream(resolve(__dirname, ))
+            // fs.createWriteStream(resolve(__dirname, ))
         });
         file.on('end', () => {
 

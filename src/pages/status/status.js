ff81fa2aede89f5d1f89e4d7434fe10845784a78
zhoubing <zb09120102ym@163.com>
2018-04-02 01:04:41 +0800

    处理审核状态页面

--- src/pages/status/status.js
+++ src/pages/status/status.js
@@ -10,6 +10,10 @@ Page({
           post_time: '2018-02-07',
           id: 2
       }]
+
+      
+
+    
     },
     onLoad: function(){
         this.getList();
@@ -26,29 +30,65 @@ Page({
         mask: true
       });
       wx.request({
-        url: 'https://www.lifuzhao100.cn/api/check/list',
-        method: 'POST',
-        success: function (res) {
-          let { errcode, data } = res.data;
+      //   url: 'https://www.lifuzhao100.cn/api/check/list',
+      //   method: 'POST',
+      //   success: function (res) {
+      //     let { errcode, data } = res.data;
 
+      //     let decodeData = data.map(obj => {
+      //       let ret = {};
+      //       for (let k in obj) {
+      //         ret[k] = decodeURIComponent(obj[k]);
+      //       }
+      //       return ret;
+      //     });
+      //     if (errcode === 0) {
+      //       that.setData({
+      //         statusList: decodeData
+      //       });
+      //     }
+        
+      //   },
+      //   fail: function (res) {
+
+      //   },
+      //   complete: function () {
+      //     wx.hideLoading();
+      //     if(typeof cb === 'function'){
+      //       cb();
+      //     }
+      //   }
+
+        url:'https://www.lifuzhao100.cn/api/check/list',
+        method:'POST',
+        
+        success: function (res){
+             
+              
+              
+          let { errcode, data } = res.data;
+        console.log(res.data.data);
           let decodeData = data.map(obj => {
             let ret = {};
+            console.log(ret);
             for (let k in obj) {
+              
               ret[k] = decodeURIComponent(obj[k]);
+              console.log(ret);
             }
             return ret;
           });
+         
           if (errcode === 0) {
             that.setData({
               statusList: decodeData
             });
           }
-        
         },
-        fail: function (res) {
-
+        fail: function fail(res){
+          
         },
-        complete: function () {
+        complete: function (){
           wx.hideLoading();
           if(typeof cb === 'function'){
             cb();
@@ -57,9 +97,24 @@ Page({
       })
     },
     viewDetail: function(e){
+        // let id = e.target.id;
+        // wx.navigateTo({
+        //     url: `../detail/detail?id=${id}`
+        // });
         let id = e.target.id;
-        wx.navigateTo({
-            url: `../detail/detail?id=${id}`
-        });
+        wx.request({
+          success:function a() {console.log(222);},
+          url: 'http://www.lifuzhao100.cn/api/wx/get_report',
+          method:'POST',
+          data:{
+            id:id,
+          },
+          success:function success(){
+
+          },
+          fail:function fail(){
+
+          }
+        })
     }
 });
